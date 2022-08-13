317217da2cdd4bc8848a3ef57278fa33c8020e04
최소희 <chosohi@naver.com>
2022-08-13 16:53:52 +0900

    Feat: coin 이미지 렌더링

--- Trello/src/routes/Coins.tsx
+++ Trello/src/routes/Coins.tsx
@@ -1,4 +1,93 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 function Coins() {
-  return <h1>Coins</h1>;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // ()() => 함수를 바로 실행할 수 있는 꿀팁
+    (async () => {
+      const response = await (await fetch("https://api.coinpaprika.com/v1/coins")).json();
+      setCoins(response.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>COIN</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>
+                <img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} alt={coin.symbol} />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 export default Coins;
+
+const Container = styled.div`
+  margin: 0px auto;
+  padding: 0px 20px;
+  max-width: 480px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  height: 10vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  display: block;
+  text-align: center;
+`;
