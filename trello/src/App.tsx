daa3648f3f1668023f6e2fdb8eb8086c57849b7d
최소희 <chosohi@naver.com>
2022-09-06 18:15:26 +0900

    [FE]:Feat: recoil selector 함수 활용한 시간계산 기능 구현

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import {useRecoilState} from 'recoil';
-import {minuteState} from './atoms';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {hourSelector, minuteState} from './atoms';
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
+  // 첫번째 argument는 get함수 반환값, 두번째 argument는 set함수
+  const [hours, setHours] = useRecoilState(hourSelector);
+
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     // string 개체앞에 +를 붙이면 number로 타입이 변함
     setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FocusEvent<HTMLInputElement>) => {
+    // hourSelector의 set함수 두번째 인자값 newValue값을 지정해줌
+    setHours(+event.currentTarget.value);
   };
   return (
     <div>
@@ -16,7 +23,12 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input type="number" placeholder="Hours" />
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
--- trello/src/atoms.ts
+++ trello/src/atoms.ts
@@ -4,3 +4,18 @@ export const minuteState = atom({
   key: 'minutes',
   default: 0,
 });
+
+export const hourSelector = selector<number>({
+  key: 'hours',
+  get: ({get}) => {
+    // get함수는 atom값을 가져오는 함수
+    const minutes = get(minuteState);
+
+    return minutes / 60;
+  },
+  // set함수는 state를 수정하는 함수
+  set: ({set}, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
+  },
+});
